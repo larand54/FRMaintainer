368545e66ebc62dbe10122f88e8f0eb4e6371eae
Lars-Göran Andersson <larand54@yahoo.se>
2014-12-08 14:41:12 +0100

    SQL-script skapat för ny tabell DocType

---
+++ Source/SQL/CREATE_DocType.sql
@@ -0,0 +1,23 @@
+USE [vis_vida]
+GO
+
+/****** Object:  Table [dbo].[DocType]    Script Date: 12/08/2014 14:08:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[DocType](
+	[id] [int] NOT NULL,
+	[name] [varchar](20) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
